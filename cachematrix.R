81d9369e348cea0cef24c71021defaf36471e5ec
Vipin <vipin251@gmail.com>
2015-03-14 21:27:26 +0530

    version 1.0

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,44 @@
-## Creating a function to cache a matrix value
-## functions do
-
-## Need to write the functions 
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#Creates a list of functions to retrieve cache value of inverse of a matrix
+#It is a constructor function
+makeCacheMatrix <- function(matrix = matrix()){
+        
+        # initial value of cache is set to NULL
+        cache <-  NULL
+        
+        #function to get the argumment matrix
+        getmatrix <- function() matrix
+        
+        #Function to retrieve inverse of the given matrix
+        getinverse <- function() cache
+        
+        #Value is assigned to the cache using <<- so the it can be used in 
+        # a different env
+        setcache <- function(inv){
+                cache <<- inv
+        }
+        
+        #returnig all the functions
+        list (get = getmatrix, 
+              setcache = setcache, 
+              getinverse = getinverse)
 }
- cache <- NULL
- set <- function(y){
-   x <<- y
-   Cache<<- NULL
- }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Funtion to compute the inverse of matrix or to retrieve already calculated 
+#inverse value
+cacheSolve  <- function(funList,...){
+        # Calling the function & Checking if the inverse is already calculated 
+        inv <- funList$getinverse()  
+        if(!is.null(inv)){
+                message("Getting inverse from Cache")
+                inv              
+        }
+        
+        #If it is not calculated before, computing the inverse
+        else{   message("Computing inverse")
+                matrix <- funList$get() #Retrieving the matrix using get func.
+                inv <- solve(matrix)   #Finding the inverse
+                funList$setcache(inv)  #Send it to the Glob env for caching.
+                return(inv)             #returning the inverse of the matrix
+                
+        }       
 }
