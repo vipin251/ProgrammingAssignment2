ff61e1f2637f20aa0971fca69eff6a69bf6dd50d
Vipin <vipin251@gmail.com>
2015-03-21 13:44:52 +0530

    Version 3.0

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,11 @@
 # Creates a list of functions to retrieve cache value of inverse of a matrix
 # It is a constructor function
+# Args : 
+#       The matrix whose inverse is to be calculated and stored in a cache
+#Returns: #  3 functions as a named list 
+#               "getmatrix" gets the argument matirx
+#               "setcachce" sets the inverse to cache
+#               "getinverse" gets in inverse value if its already computed
 makeCacheMatrix <- function(matrix = matrix()){
         
         # Set initial value of cache to NULL
@@ -8,14 +14,16 @@ makeCacheMatrix <- function(matrix = matrix()){
         # Function to get the matrix
         getmatrix <- function() matrix
         
-        # Function to retrieve inverse of the given matrix
-        getinverse <- function() cache
-        
         # Value is assigned to the cache using <<- so the it can be used in 
         # a different env
         setcache <- function(inv){
                 cache <<- inv
         }
+        
+        # Function to retrieve inverse of the given matrix
+        getinverse <- function() cache
+        
+     
         
         # Returns all the functions as a list with specific names
         list (get = getmatrix, 
@@ -24,17 +32,19 @@ makeCacheMatrix <- function(matrix = matrix()){
 }
 
 # Funtion to compute the inverse of matrix or to retrieve already calculated 
-# inverse value
+# Args:
+#       Name of the created list
+#Retruns: 
+#       Inverse of the matrix
 cacheSolve  <- function(funList,...){
-        # Call the function & Check if the inverse is already calculated 
+        # Check if the inverse is already calculated 
         inv <- funList$getinverse()  
-        if(!is.null(inv)){
+        if (!is.null(inv)){
                 message("Getting inverse from Cache")
                 inv              
-        }
-        
-        # If it is not calculated before, compute the inverse
-        else{   message("Computing inverse")
+        } else {
+                #Compute the inverese
+                message("Computing inverse")
                 matrix <- funList$get() # Retrieve the matrix using get func.
                 inv <- solve(matrix)   # Find the inverse
                 funList$setcache(inv)  # Send to the Glob env for caching.
